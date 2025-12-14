700858b7738858e3585a63336ca50a0f7e8a573c
Emre MENTES <emreowen57@gmail.com>
2025-12-14 15:08:10 +0100

    changes

--- src/test/java/stepdefs/GridStepDefs.java
+++ src/test/java/stepdefs/GridStepDefs.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
-
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.time.Duration;
@@ -23,40 +22,30 @@ public class GridStepDefs {
     @Given("kullanici {string} adresine {string} ile gider")
     public void kullanici_adresine_ile_gider(String url, String browser) throws URISyntaxException, MalformedURLException {
 
-        URL remoteUrl = new URL("http://192.168.2.103:4444");
+        // Jenkins ile çalışmak için localhost
+        URL remoteUrl = new URL("http://localhost:4444");
 
         if (browser.equalsIgnoreCase("chrome")){
             driver = new RemoteWebDriver(remoteUrl, new ChromeOptions());
-
         } else if (browser.equalsIgnoreCase("firefox")) {
             driver = new RemoteWebDriver(remoteUrl, new FirefoxOptions());
-
         } else if (browser.equalsIgnoreCase("edge")) {
             driver = new RemoteWebDriver(remoteUrl, new EdgeOptions());
         }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-
-
-
         driver.get(url);
-
-
-
-
     }
+
     @When("kullanici basligin {string} icerdigin dogrula")
     public void kullanici_basligin_icerdigin_dogrula(String title) {
         System.out.println("driver.getTitle() = " + driver.getTitle());
         Assert.assertTrue(driver.getTitle().contains(title));
-
-
     }
+
     @Then("kullanici sayfayi kapatir")
     public void kullanici_sayfayi_kapatir() {
         driver.quit();
-;
     }
-
-
 }
