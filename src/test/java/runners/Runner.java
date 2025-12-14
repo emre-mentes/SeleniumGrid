f3e4cdec878cc2df96c2655def06204f164206a9
Emre MENTES <emreowen57@gmail.com>
2025-12-14 22:47:22 +0100

    changes

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -1,29 +1,23 @@
 package runners;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
-       //plugin = {
-       //        "pretty",//console da scenariolar ile ilgili ayrintili bilgi verir
-       //        "html:target/default-cucumber-reports.html",
-       //        "json:target/json-reports/cucumber.json",
-       //        "junit:target/xml-report/cucumber.xml",
-       //        "rerun:TestOutput/failed_scenario.txt"
-       //},
         features = "src/test/resources/features",
         glue = {"stepdefs"},
         tags = "@grid",
+        plugin = {
+                "pretty",
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml",
+                "rerun:TestOutput/failed_scenario.txt"
+        },
         dryRun = false,
         monochrome = false
-
 )
-
 public class Runner {
-
-
 }
